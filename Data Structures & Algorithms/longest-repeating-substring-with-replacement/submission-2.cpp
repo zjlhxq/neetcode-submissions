19c67e4c868fdfb42b7858ea380b1ca422629395
Julian Z <hadoop.jz@qq.com>
2026-08-02 12:02:51 -0400

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> count;
+        int left = 0;
+        int max_length = 0;
+        int max_freq_count = 0;
+        for(int right = 0; right < s.length(); right ++) {
+            max_freq_count = max(max_freq_count, ++count[s[right]]);
+
+            int cur_length = right - left + 1;
+            if(cur_length - max_freq_count > k) { // breaks the condition
+                count[s[left]] --;
+                left ++;
+            }
+
+            max_length = max(max_length, right - left + 1);
+        }
+
+        return max_length;
+    }  
+};
