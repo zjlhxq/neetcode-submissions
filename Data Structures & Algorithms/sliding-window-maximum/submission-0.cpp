3c4992a9b45e321ef4d0414a22da21731f3b0e99
Julian Z <hadoop.jz@qq.com>
2026-08-02 18:38:17 -0400

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq; // for indices
+        vector<int> res;
+        for(int r = 0; r < nums.size(); r ++) {
+
+            // remove element indices that are out of the window
+            while(!dq.empty() && dq.front() <= r - k) {
+                dq.pop_front();
+            }
+
+            // ensure descreasing elements
+            while(!dq.empty() && nums[dq.back()] <= nums[r]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(r);
+
+            if (r + 1 >= k) {
+                res.push_back(nums[dq.front()]);
+            }
+
+        }
+
+        return res;
+    }
+};
