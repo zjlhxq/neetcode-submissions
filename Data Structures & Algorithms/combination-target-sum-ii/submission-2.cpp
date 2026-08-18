41362f1ba09d1c1ce4208da4e20ec0aa8e3c1840
Julian Z <hadoop.jz@qq.com>
2026-08-18 11:52:56 -0400

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> path;
+    
+        sort(candidates.begin(), candidates.end());
+        function<void(int,int)> backtrack = [&](int start, int remaining) {
+            if(remaining == 0) {
+                result.push_back(path);
+            }
+
+
+
+            for(int i=start; i<candidates.size(); i++) {
+                if( i> start && candidates[i] == candidates[i-1]) {
+                    continue;
+                }
+
+                if(remaining < candidates[start]) {
+                    return;
+                }
+
+                path.push_back(candidates[i]);
+                backtrack(i+1, remaining - candidates[i]);
+                path.pop_back();
+            }
+        };
+
+        backtrack(0, target);
+
+        return result;
+
+    }
+};
