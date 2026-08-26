bd36886c72d237342d2d42ce0395547717286193
Julian Z <hadoop.jz@qq.com>
2026-08-26 00:14:43 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+     priority_queue<int> pq;
+
+     for(int stone : stones) {
+        pq.push(stone);
+     }
+
+     while(pq.size() > 1) {
+        int x = pq.top();
+        pq.pop();
+
+        int y = pq.top();
+        pq.pop();
+        
+        if( x != y ) {
+            pq.push(x - y);
+        } 
+     }
+
+     return pq.empty() ? 0 : pq.top();
+
+    }
+};
