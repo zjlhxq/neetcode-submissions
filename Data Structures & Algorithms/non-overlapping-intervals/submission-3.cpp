d0f9b0682932e7fefcfa6ddf23421f00b9830cd3
Julian Z <hadoop.jz@qq.com>
2026-08-30 19:58:16 -0400

    Add: non-overlapping-intervals - submission-3

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a[1] < b[1];
+        } );
+
+        int prev_end = INT_MIN;
+        int removed = 0;
+        for(auto& interval : intervals) {
+            if(interval[0] >= prev_end) {
+                prev_end = interval[1];    
+            } else {
+                removed ++;
+            }
+        }
+
+        return removed;
+    }
+};
