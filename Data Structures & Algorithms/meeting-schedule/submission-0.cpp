df06689738fcf3285f17d98901f88224ae84b217
Julian Z <hadoop.jz@qq.com>
2026-08-29 22:22:47 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto & a, auto&b){return a.start < b.start;});
+        
+        for(int i=1; i<intervals.size(); i++) {
+            if(intervals[i].start < intervals[i-1].end) return false;
+        }
+
+        return true;
+    }
+};
